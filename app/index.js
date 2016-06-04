3e974d4f49ca145c71bcdb7c98615af491e35c8d
Xu Gao <xugao0131@hotmail.com>
2016-06-04 10:27:05 -0700

    integrate datepicker

--- app/index.js
+++ app/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import 'react-date-picker/index.css';
+import DatePicker from 'react-date-picker';
+
 class App extends React.Component {
+  onChange(dateString, { dateMoment, timestamp }) {
+    console.log(dateString);
+  }
+
   render () {
     return (
         <div>
             <p>Hello React!</p>
+            <DatePicker
+              minDate={Date.now()}
+              maxDate='2020-1-1'
+              date={Date.now()}
+              onChange={this.onChange}
+            />
         </div>
     );
   }
